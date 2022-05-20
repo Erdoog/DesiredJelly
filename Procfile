976636cfd6ba6ed4fcd21df43734cb4f56c8eddf
Erdog Utyugov <nimdig@gmail.com>
2022-05-21 00:11:59 +0600

    added a status changer, attempted to fix voice log

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 worker: ts-node index
-installer: npm install -g typescript ts-node
--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import * as DiscordJS from 'discord.js';
 import * as dotenv from 'dotenv';
-import { Intents } from "discord.js";
+import { Intents, Constants } from "discord.js";
 dotenv.config();
 
 let imperator : DiscordJS.User;
-let defaultprefix = '*';
-let prefixes :{[key: string]: string[]} = {}; //   { 'serverid' : [prefix0, prefix1...]}
+const mGuildId = '732947430080774184';
+let defaultprefix = '#';
+let prefixes: {[key: string]: string[]} = {}; //   { 'serverid' : [prefix0, prefix1...]}
+let commandopts: {[key: string]: {[key: string] : any[]}} = {};
 
 let permittedusrs : Array<DiscordJS.User> = [];
 
@@ -20,11 +22,14 @@ const client = new DiscordJS.Client({
 });
 
 client.on('ready', () => {
-    imperator = client?.users?.cache?.get('534032368634167311')!;
-    permittedusrs.push(imperator);
+    console.log("Logged!");
+    client.users.fetch('534032368634167311').then(async (user) => {
+        imperator = user;
+        permittedusrs.push(user);
+    });
     client.guilds.cache.forEach((guild) => {
         prefixes[guild.id] = [defaultprefix,];
-    })
+    });
     client?.user?.setPresence(
         {
             status: 'online',
@@ -35,9 +40,142 @@ client.on('ready', () => {
                 }
             ]
         }
-    )
-    console.log("Logged!");
+    );
+    const guild = client.guilds.cache.get(mGuildId)!;
+    let commands;
+    if (guild)
+        commands = guild.commands;
+    else
+        commands = client.application?.commands;
+    commands?.create({
+        name: 'changeactivity',
+        description: 'Изменить активность бота', 
+        options: [
+            {
+                name: 'subject',
+                description: 'Субъект активности',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING
+            },
+        ],
+    });
+    // guild.commands.cache.forEach((value) => {
+    //     value.delete()
+    // })
+    console.log('Steady and ready!');
 });
+
+client.on('interactionCreate', async (interaction) => {
+    if (interaction.isCommand())
+    {
+        await interaction.deferReply({
+            ephemeral: true
+        });
+        const { commandName, options } = interaction;
+        if (!interaction.member)
+        {
+            interaction.reply('Your user is null for some reason');
+            return;
+        }
+        if (!commandopts[interaction.member.user.id])
+            commandopts[interaction.member.user.id] = {};
+        if (commandopts[interaction.member.user.id][commandName] !== undefined){
+            interaction.reply('Нихуа ты разогнался, это нельзя вызвать несколько раз одновременно');
+            return;
+        }
+        if (commandName === 'changeactivity')
+        {
+            commandopts[interaction.member!.user.id]['changeactivity'] = [options.getString('subject', true)];
+            const typerow = new DiscordJS.MessageActionRow().addComponents(
+                new DiscordJS.MessageSelectMenu()
+                .setCustomId('actntype')
+                .setPlaceholder('Выберите тип активности')
+                .addOptions([
+                    {
+                        label: 'Играть',
+                        description: 'Пример: Играет в Тарков',
+                        value: 'PLAYING',
+                    },
+                    {
+                        label: 'Смотреть',
+                        description: 'Пример: Смотрит Тарков',
+                        value: 'WATCHING',
+                    },
+                    {
+                        label: 'Стримить',
+                        description: 'Пример: Стримит Тарков',
+                        value: 'STREAMING',
+                    },
+                    {
+                        label: 'Слушать',
+                        description: 'Пример: Слушает Тарков',
+                        value: 'LISTENING',
+                    },
+                    {
+                        label: 'Соревноваться',
+                        description: 'Пример: Соревнуется в Тарков',
+                        value: 'COMPETING',
+                    }
+                ])
+            )
+            if (!interaction.deferred){
+                // interaction.reply('wtf');
+                return;
+            }
+            await interaction.editReply(
+                {
+                    content: 'Теперь определи как будет выглядеть активность',
+                    components: [typerow],
+                }
+            )
+            return;
+
+        }
+    } else if (interaction.isSelectMenu())
+    {
+        if (interaction.customId === 'actntype')
+        {
+            let typee = Constants.ActivityTypes.PLAYING;
+            switch(interaction.values[0])
+            {
+                case 'COMPETING':
+                    typee = Constants.ActivityTypes.COMPETING;
+                    break;
+                case 'PLAYING':
+                    typee = Constants.ActivityTypes.PLAYING;
+                    break;
+                case 'LISTENING':
+                    typee = Constants.ActivityTypes.LISTENING;
+                    break;
+                case 'WATCHING':
+                    typee = Constants.ActivityTypes.WATCHING;
+                    break;
+                case 'STREAMING':
+                    typee = Constants.ActivityTypes.STREAMING;
+                    break;
+            }
+            if (typeof commandopts[interaction.member!.user.id]['changeactivity'][0] !== 'string')
+            {
+                interaction.update('Error');
+                return;
+            }
+            client.user?.setActivity({
+                name: commandopts[interaction.member!.user.id]['changeactivity'][0],
+                type: typee,
+            });
+            delete commandopts[interaction.member!.user.id]['changeactivity'];
+            await interaction.update({
+                content: 'Статус успешно обновлён!',
+                components: []
+            });
+        }
+    }
+})
+
+// client.user?.setActivity({
+//     name: options.getString('subject', true),
+//     type: 'PLAYING',
+// });
 
 client.on('guildCreate', (guild) => {
     console.log(guild.name);
@@ -134,10 +272,10 @@ client.on('messageCreate', (message) => {
             prefixes[message?.guild?.id!].splice(eltodeleteind, 1);
             if (prefixes[message?.guild?.id!].length != 0)
             {
-                message.reply(`Префикс ${args[2]} истреблён, остались ${getPrefixes(message?.guild?.id!)}`);
+                message.reply(`Префикс \`${args[2]}\` истреблён, остались ${getPrefixes(message?.guild?.id!)}`);
                 return;
             }
-            message.reply(`Префикс ${args[2]} истреблён, все префиксы ушли в ислам`);
+            message.reply(`Префикс \`${args[2]}\` истреблён, все префиксы ушли в ислам`);
             return;
         }
         if (args[1] == 'set')
@@ -163,8 +301,8 @@ let voiceConfig = {
     inkid: '702721820499116054',
     killaid: '398132164182671361',
     // was just testing out on my accounts
-    // let inkid = '534032368634167311';
-    // let killaid = '716276860660613150';
+    // inkid: '534032368634167311',
+    // killaid: '716276860660613150',
 
     // i have no idea what this is
     // killamsgid,
@@ -178,15 +316,13 @@ function getPrefixes(guildid : string) : string
     return prefixesres.slice(0, -2);
 }
 
-client.on('voiceStateUpdate', (newVoiceState) => {
+client.on('voiceStateUpdate', async (newVoiceState) => {
     if (newVoiceState.id != voiceConfig.inkid && newVoiceState.id != voiceConfig.killaid)
         return;
-    let actor = client.users.cache.get(newVoiceState.id);
+    let actor : DiscordJS.User = client.users.cache.get(newVoiceState.id)!;
     // console.log(newVoiceState);
     // console.log(`${imperator?.username} and ${actor?.username}`);
-    if (!actor)
-        imperator?.send('U fucked up');
-    if (newVoiceState.id == voiceConfig.inkid)
+    if (actor.id == voiceConfig.inkid)
     {
         if (voiceConfig.inkin)
         {
@@ -197,7 +333,7 @@ client.on('voiceStateUpdate', (newVoiceState) => {
             voiceConfig.inkin = true;
             imperator?.send("Inkie is waiting!");
         }
-    } else if (newVoiceState.id == voiceConfig.killaid)
+    } else if (actor.id == voiceConfig.killaid)
     {
         if (voiceConfig.killain)
             voiceConfig.killain = false;
