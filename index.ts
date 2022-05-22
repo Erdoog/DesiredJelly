7eb9fe503bf558e8ffc76f1cb4a62881faa171d7
Erdoog <nimdig@gmail.com>
2022-05-22 14:57:08 +0600

    fixed reply on unsopperted commands

--- index.ts
+++ index.ts
@@ -296,7 +296,7 @@ client.on('messageCreate', (message) => {
         message.reply(`Мои префиксы:   ${getPrefixes(message?.guild?.id!)}`);
         return;
     }
-    message.reply('Ты не указал команду, разраб пока что слишком ленив чтобы добавить команду help');
+    message.reply('Команда которую ты указал не поддерживается, разраб пока что слишком ленив чтобы добавить команду help');
 })
 
 let voiceConfig = {
